8a9e42c69fdc86dfcd10861d132346bd044ddc18
Anthony Arabia <tjay15787@gmail.com>
2022-12-08 09:44:20 +0100

    updated first class and created hello world class

--- src/First.java
+++ src/First.java
@@ -1,5 +1,5 @@
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is the first java class of the project");
     }
 }
